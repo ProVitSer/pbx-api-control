7262c555bd9514bb7be355b2ee43133d6f60a5f9
Vitaliy <prokinvs@gmail.com>
2023-12-15 21:19:53 +0100

    test: controller for test pbx method

---
+++ PbxApiControl/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using TCX.Configuration;
+
+
+namespace PbxApiControl.Controllers
+{
+
+    [ApiController]
+    [Route("api")]
+    public class TestController : ControllerBase
+    {
+
+
+        [HttpGet("ext.info.get", Name = "GetExtInfo")]
+        public void GetExtInfo()
+        {
+
+            using (DN dnByNumber = PhoneSystem.Root.GetDNByNumber((string)"101"))
+            {
+
+                Console.WriteLine(dnByNumber is Extension extension);
+                if (dnByNumber is Extension ext)
+                {
+                    Console.WriteLine(ext.EmailAddress);
+                };
+            }
+
+        }
+
+        [HttpGet("makecall", Name = "MakeCall")]
+        public void MakeCall()
+        {
+
+            PhoneSystem.Root.MakeCall((string)"101", "79104061420");
+        }
+    }
+}

47d2b7a48582183ea33726c73014da63dc014444
Vitaliy <prokinvs@gmail.com>
2023-12-16 21:26:42 +0100

    stop pbx event listener

--- PbxApiControl/Program.cs
+++ PbxApiControl/Program.cs
@@ -3,32 +3,32 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 
-namespace PbxApiControl
+namespace PbxApiControl;
+#nullable enable
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+
+        try
         {
+            CreateHostBuilder(args).Build().Run();
 
-            try
-            {
-                CreateHostBuilder(args).Build().Run();
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw new Exception("Проблемы запуска проекта");
-            }
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                    PBXAPIConfig.InitConfig();
-                    PbxEventListener.Start();
-                });
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception("Проблемы запуска проекта");
+        }
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+                PBXAPIConfig.InitConfig();
+                //PbxEventListener.Start();
+            });
 }

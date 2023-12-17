c58ecad72d49a9b43f25a78aa04a59bae0d6b93c
Vitaliy <prokinvs@gmail.com>
2023-12-17 19:26:30 +0100

    ref

---
+++ PbxApiControl/DTOs/CallsDto.cs
@@ -0,0 +1,10 @@
+namespace PbxApiControl.DTOs
+{
+    public class CallsDto
+    {
+        public int currentCountCalls { get; set; } = 0;
+
+    }
+}
+
+
--- PbxApiControl/DTOs/Pbx/CallsDto.cs
+++
@@ -1,7 +0,0 @@
-namespace PbxApiControl.DTOs.Pbx;
-
-public class CallsDto
-{
-    public int currentCountCalls { get; set; } = 0;
-
-}

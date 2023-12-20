2ed09a150a772857f74ba52afdc22a90bb23e34c
Vitaliy <prokinvs@gmail.com>
2023-12-20 21:43:33 +0100

    feat: add ExtensionService

--- PbxApiControl/Extensions/ApplicationServiceExtensions.cs
+++ PbxApiControl/Extensions/ApplicationServiceExtensions.cs
@@ -8,12 +8,10 @@ public static class ApplicationServiceExtensions
 {
     public static void AddApplicationServices(this IServiceCollection services)
     {
-
         services.AddScoped<IPbxService, PbxService>();
         services.AddScoped<IGetActiveConnectionService, GetActiveConnectionsService>();
         services.AddScoped<ICallService, CallService>();
-
-
+        services.AddScoped<IExtensionService, ExtensionService>();
     }
 
 }

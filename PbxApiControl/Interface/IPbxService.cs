3b5c0e952e6172c141f3c0860812e40abbb39b7e
Vitaliy <prokinvs@gmail.com>
2023-12-20 21:44:52 +0100

    feat: add get pbx number by type

--- PbxApiControl/Interface/IPbxService.cs
+++ PbxApiControl/Interface/IPbxService.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using PbxApiControl.DTOs.Pbx;
+using TCX.Configuration;
 
 
 namespace PbxApiControl.Interface;
 public interface IPbxService
 {
     PbxCountCallsDto PbxCountCalls();
+    IEnumerable<string> GetPbxNumbers<T>() where T : class, DN;
 
 }

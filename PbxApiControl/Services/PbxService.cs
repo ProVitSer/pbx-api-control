3665be11018e9e9cd3b00f292a7e9dbd3ea894ac
Vitaliy <prokinvs@gmail.com>
2023-12-20 21:44:05 +0100

    feat: add get pbx number by type

--- PbxApiControl/Services/PbxService.cs
+++ PbxApiControl/Services/PbxService.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using PbxApiControl.DTOs.Pbx;
 using PbxApiControl.Interface;
 using TCX.Configuration;
@@ -7,6 +9,14 @@ namespace PbxApiControl.Services;
 #nullable enable
 public class PbxService : IPbxService
 {
+
+    public IEnumerable<string> Numbers { get; private set; }
+
+    public PbxService()
+    {
+        Numbers = new List<string>();
+    }
+
     public PbxCountCallsDto PbxCountCalls()
     {
         var countCalls = PhoneSystem.Root.GetActiveConnectionsByCallID().Count;
@@ -15,5 +25,16 @@ public class PbxService : IPbxService
             currentCountCalls = countCalls
         };
     }
+    public IEnumerable<string> GetPbxNumbers<T>() where T : class, DN
+    {
+
+        using (IArrayDisposer<T> disposer = PhoneSystem.Root.GetAll<T>().GetDisposer())
+        {
+            this.Numbers = disposer.Select(x => x.Number).ToArray();
+
+            return this.Numbers;
+        };
+    }
+
 }
 

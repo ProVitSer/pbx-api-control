c4290d35f7ff45b4c2e9fe324fe0819fe8d2cddc
Vitaliy <prokinvs@gmail.com>
2023-12-21 22:17:12 +0100

    feat: add util service for common method

---
+++ PbxApiControl/Services/UtilService.cs
@@ -0,0 +1,29 @@
+using System.Security.Cryptography;
+
+namespace PbxApiControl
+{
+
+    public static class UtilService
+    {
+
+        public static string GeneratePassword(int length = 12)
+        {
+            const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-=_+";
+
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                byte[] bytes = new byte[length];
+                rng.GetBytes(bytes);
+
+                char[] chars = new char[length];
+                for (int i = 0; i < length; i++)
+                {
+                    chars[i] = validChars[bytes[i] % validChars.Length];
+                }
+
+                return new string(chars);
+            };
+        }
+
+    }
+}

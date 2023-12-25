2909361fdc633dee541bac30dd1d377c87a12ce8
Vitaliy <prokinvs@gmail.com>
2023-12-25 20:55:07 +0100

    feat: add pbx method GetExtensionStatus

--- PbxApiControl/Interface/IPbxService.cs
+++ PbxApiControl/Interface/IPbxService.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using PbxApiControl.DTOs.Pbx;
+using PbxApiControl.Enums;
 using TCX.Configuration;
 
 
@@ -8,5 +9,5 @@ public interface IPbxService
 {
     PbxCountCallsDto PbxCountCalls();
     IEnumerable<string> GetPbxNumbers<T>() where T : class, DN;
-
+    ExtStatus GetExtensionStatus(string ext);
 }
--- PbxApiControl/Services/PbxService.cs
+++ PbxApiControl/Services/PbxService.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using PbxApiControl.DTOs.Pbx;
+using PbxApiControl.Enums;
 using PbxApiControl.Interface;
 using TCX.Configuration;
 
@@ -33,6 +34,20 @@ public class PbxService : IPbxService
             this.Numbers = disposer.Select(x => x.Number).ToArray();
 
             return this.Numbers;
+        };
+    }
+
+    public ExtStatus GetExtensionStatus(string ext)
+    {
+        using (DN dnByNumber = PhoneSystem.Root.GetDNByNumber(ext))
+        {
+            if (dnByNumber == null || !(dnByNumber is Extension)) return ExtStatus.DoesNotExists;
+
+            using (IArrayDisposer<ActiveConnection> disposer = dnByNumber.GetActiveConnections().GetDisposer())
+            {
+                return disposer.Length == 0 ? ExtStatus.Idle : ExtStatus.Busy;
+            };
+
         };
     }
 

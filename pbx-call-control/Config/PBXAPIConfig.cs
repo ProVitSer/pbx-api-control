591e6595abe7b3b5de6cc146d3b6bffc8fea1449
Vitaliy <prokinvs@gmail.com>
2024-06-17 15:41:26 +0200

    ref: exteption desc and init conf

--- pbx-call-control/Config/PBXAPIConfig.cs
+++ pbx-call-control/Config/PBXAPIConfig.cs
@@ -1,13 +1,12 @@
 ﻿using System.Reflection;
 using TCX.Configuration;
 
-
 namespace PbxApiControl.Config
 {
-
     public static class PBXAPIConfig
     {
         public static string? instanceBinPath;
+
         public static void InitConfig()
         {
             try
@@ -15,25 +14,19 @@ namespace PbxApiControl.Config
                 Dictionary<string, Dictionary<string, string>> iniContent = new Dictionary<string, Dictionary<string, string>>((IEqualityComparer<string>)StringComparer.InvariantCultureIgnoreCase);
                 Dictionary<string, Dictionary<string, string>> Content = new Dictionary<string, Dictionary<string, string>>((IEqualityComparer<string>)StringComparer.InvariantCultureIgnoreCase);
 
-                PBXAPIConfig.ReadConfiguration(iniContent, "api.ini");
+                ReadConfiguration(iniContent, "api.ini");
+                ReadConfiguration(Content, Path.Combine(iniContent["General"]["PBX_INI_PATH"], "3CXPhoneSystem.ini"));
 
-                PBXAPIConfig.ReadConfiguration(Content, Path.Combine(iniContent["General"]["PBX_INI_PATH"], "3CXPhoneSystem.ini"));
+                instanceBinPath = Path.Combine(Content["General"]["AppPath"], "Bin");
 
-                PBXAPIConfig.instanceBinPath = Path.Combine(Content["General"]["AppPath"], "Bin");
-
-                AppDomain.CurrentDomain.AssemblyResolve += PBXAPIConfig.CurrentDomain_AssemblyResolve;
+                AppDomain.CurrentDomain.AssemblyResolve += (sender, args) => CurrentDomain_AssemblyResolve(args, instanceBinPath);
 
                 ConnectToTCX(Content);
-
             }
             catch (Exception ex)
-
             {
-                Console.WriteLine(ex);
-
-                throw new Exception("Проблемы с инициализацией");
+                throw new Exception("Проблемы с инициализацией: " + ex.Message, ex);
             }
-
         }
 
         private static void ConnectToTCX(Dictionary<string, Dictionary<string, string>> Content)
@@ -53,9 +46,9 @@ namespace PbxApiControl.Config
 
                 ps.WaitForConnect(TimeSpan.FromSeconds(5.0));
             }
-            catch
+            catch (Exception ex)
             {
-                throw new Exception("Проблемы с подключение к АТС");
+                throw new Exception("Проблемы с подключение к АТС: " + ex.Message, ex);
             }
         }
 
@@ -68,7 +61,7 @@ namespace PbxApiControl.Config
 
             string[] strArray = File.ReadAllLines(filePath);
 
-            Dictionary<string, string> dictionary = (Dictionary<string, string>)null;
+            Dictionary<string, string> dictionary = null;
 
             for (int index = 1; index < strArray.Length + 1; ++index)
             {
@@ -76,23 +69,33 @@ namespace PbxApiControl.Config
 
                 if (str1.StartsWith("["))
                 {
-                    string str2 = str1.Split(new char[2] { '[', ']' }, (StringSplitOptions)1)[0];
+                    string str2 = str1.Split(new char[2] { '[', ']' }, StringSplitOptions.RemoveEmptyEntries)[0];
                     dictionary = Content[str2] = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
                 }
                 else if (dictionary != null && !string.IsNullOrWhiteSpace(str1) && !str1.StartsWith("#") && !str1.StartsWith(";"))
                 {
-                    string[] array = Enumerable.ToArray(Enumerable.Select((IEnumerable<string>)str1.Split("=", (StringSplitOptions)0), (Func<string, string>)(x => x.Trim())));
+                    string[] array = str1.Split("=", StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
                     dictionary[array[0]] = array[1];
                 }
             }
         }
 
-        static Assembly CurrentDomain_AssemblyResolve(object? sender, ResolveEventArgs args)
+        static Assembly CurrentDomain_AssemblyResolve(ResolveEventArgs args, string? instanceBinPath)
         {
             var name = new AssemblyName(args.Name).Name;
 
-            return Assembly.LoadFrom(Path.Combine(instanceBinPath ?? "", name + ".dll"));
+            var assemblyPath = Path.Combine(instanceBinPath ?? "", name + ".dll");
 
+            if (File.Exists(assemblyPath))
+            {
+                Console.WriteLine($"Assembly found at path: {assemblyPath}");
+                return Assembly.LoadFrom(assemblyPath);
+            }
+            else
+            {
+                Console.WriteLine($"Assembly not found at path: {assemblyPath}");
+                return null;
+            }
         }
     }
 }

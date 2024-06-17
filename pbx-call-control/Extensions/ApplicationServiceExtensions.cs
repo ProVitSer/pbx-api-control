0c51a4a67446d459a1b8c1cd23250d2432525123
Vitaliy <prokinvs@gmail.com>
2024-06-17 15:39:03 +0200

    fix: Culture ru tu us, add host from conf

--- pbx-call-control/Extensions/ApplicationServiceExtensions.cs
+++ pbx-call-control/Extensions/ApplicationServiceExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using PbxApiControl.Services.Pbx;
 using PbxApiControl.Interface;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace PbxApiControl.Extensions;
 
@@ -8,7 +11,23 @@ public static class ApplicationServiceExtensions
     public static void AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<IExtensionService, ExtensionService>();
+        services.AddLocalization(options => options.ResourcesPath = "Resources");
 
+        services.AddControllersWithViews()
+            .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+            .AddDataAnnotationsLocalization();
+
+        services.Configure<RequestLocalizationOptions>(options =>
+        {
+            var supportedCultures = new[]
+            {
+                new CultureInfo("en-US"),
+            };
+
+            options.DefaultRequestCulture = new RequestCulture("en-US");
+            options.SupportedCultures = supportedCultures;
+            options.SupportedUICultures = supportedCultures;
+        });
     }
 
 }
--- pbx-call-control/Program.cs
+++ pbx-call-control/Program.cs
@@ -5,6 +5,9 @@ using Serilog;
 using Serilog.Templates;
 using Serilog.Templates.Themes;
 using PbxApiControl.Logging;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
 
 namespace PbxApiControl
 {
@@ -12,38 +15,68 @@ namespace PbxApiControl
     {
         public static void Main(string[] args)
         {
+            RunApplication(args);
+        }
+        
+        public static void RunApplication(string[] args)
+        {
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+            
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+            
             var builder = WebApplication.CreateBuilder(args);
 
+            // Add services to the container.
             builder.Services.AddGrpc(options =>
             {
                 options.Interceptors.Add<LoggingInterceptor>();
-                
             }).AddJsonTranscoding();
-
+            
+            // Initialize configuration
+            PBXAPIConfig.InitConfig();
+            
             builder.Services.AddGrpcReflection();
-
+            
             builder.Services.AddApplicationServices();
 
             builder.Services.AddSerilog((services, lc) => lc
-           .ReadFrom.Configuration(builder.Configuration)
-           .ReadFrom.Services(services)
-           .Enrich.FromLogContext()
-           .WriteTo.Console(new ExpressionTemplate(
-               "[{@t:HH:mm:ss} {@l:u3}{#if @tr is not null} ({substring(@tr,0,4)}:{substring(@sp,0,4)}){#end}] {@m}\n{@x}",
-           theme: TemplateTheme.Code)));
+               .ReadFrom.Configuration(builder.Configuration)
+               .ReadFrom.Services(services)
+               .Enrich.FromLogContext()
+               .WriteTo.Console(new ExpressionTemplate(
+                   "[{@t:HH:mm:ss} {@l:u3}{#if @tr is not null} ({substring(@tr,0,4)}:{substring(@sp,0,4)}){#end}] {@m}\n{@x}",
+                   theme: TemplateTheme.Code)));
+            
+
+            builder.Services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[] { new CultureInfo("en-US") };
+                options.DefaultRequestCulture = new RequestCulture("en-US");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
 
             var app = builder.Build();
 
-            app.Urls.Add("http://192.168.0.2:5000");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
 
-            app.MapGrpcService<ExtensionService>();
+            string kestrelUrl = configuration["Kestrel:EndpointDefaults:Url"];
             
-            app.MapGrpcReflectionService();
+            // Add URL bindings
+            app.Urls.Add(kestrelUrl);
 
-            PBXAPIConfig.InitConfig();
+            // Localization
+            var localizationOptions = app.Services.GetService<IOptions<RequestLocalizationOptions>>().Value;
+            app.UseRequestLocalization(localizationOptions);
+
+            // Map gRPC services
+            app.MapGrpcService<ExtensionService>();
+            app.MapGrpcReflectionService();
 
             app.Run();
         }
-
     }
 }
